1b99ffa9d59a3e0a4803e74ffacbb315268d7a8b
ma2792315413@qq.com <2792315413ma@>
2026-07-22 21:15:31 +0800

    两个课堂遗留代码+课后任务

--- 1_yubingrui/40.py
+++
@@ -1,7 +0,0 @@
-#题目：字符串排序。  
-def sort_str(s):
-    return ''.join(sorted(s))
-
-if __name__ == "__main__":
-    s = input("请输入一个字符串：")
-    print(f"排序后的字符串为：{sort_str(s)}")
--- 1_yubingrui/test.py
+++
@@ -1,4 +0,0 @@
-def zuida(m,n):
-    while n != 0:
-        m,n = n,m%n
-        
